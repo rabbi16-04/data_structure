dc8b95473f754075e463407cd28f9a6bea51f0fa
Fazley Rabbi <fazleyrabbi555@gmail.com>
2018-05-10 03:18:04 +0600

    a function to insert element in bst

--- insert_BST.cpp
+++ insert_BST.cpp
@@ -2,3 +2,29 @@
 #include <iostream>
 #include <queue>
 using namespace std;
+
+/*function to insert element in binary tree */
+void insert(struct Node* temp, int key)
+{
+    queue<struct Node*> q;
+    q.push(temp);
+ 
+    // Do level order traversal until we find
+    // an empty place. 
+    while (!q.empty()) {
+        struct Node* temp = q.front();
+        q.pop();
+ 
+        if (!temp->left) {
+            temp->left = newNode(key);
+            break;
+        } else
+            q.push(temp->left);
+ 
+        if (!temp->right) {
+            temp->right = newNode(key);
+            break;
+        } else
+            q.push(temp->right);
+    }
+}
