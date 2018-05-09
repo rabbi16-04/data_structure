f5e6f40f473ced4629ebcfb0fd2ec05373146be8
Fazley Rabbi <fazleyrabbi555@gmail.com>
2018-05-10 03:08:38 +0600

    adding a binary tree node

--- insert_BST.cpp
+++ insert_BST.cpp
@@ -2,3 +2,10 @@
 #include <iostream>
 #include <queue>
 using namespace std;
+
+/* A binary tree node has key, pointer to left child
+and a pointer to right child */
+struct Node {
+    int key;
+    struct Node* left, *right;
+};
