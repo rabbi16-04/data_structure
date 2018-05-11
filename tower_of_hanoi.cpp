e8812bb1e74368b998d3179abd4600aa1d1a4209
Fazley Rabbi <fazleyrabbi555@gmail.com>
2018-05-12 04:18:42 +0600

    a function added

--- tower_of_hanoi.cpp
+++ tower_of_hanoi.cpp
@@ -1,3 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void tower_hannoi (int disk, char tower1, char tower2, char tower3)
+{
+    if (disk == 1)
+    {
+        cout << "Move disk " << disk << " from " << tower1 << " to " << tower3 << endl;}
+    else
+    {
+        tower_hannoi (disk-1, tower1, tower3, tower2);
+        cout << "Move disk " << disk << " from " << tower1 << " to " << tower3 << endl;
+        tower_hannoi (disk-1, tower2, tower1, tower3);
+    }
+}
